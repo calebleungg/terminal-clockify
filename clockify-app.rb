831ec98becb43bb6fe8eb3cd603d1dfca2a4e7f0
Caleb Leung <caleb.leungg@gmail.com>
2019-11-21 16:19:48 +1000

    adding display feature for list- dynamic time is working

--- clockify-app.rb
+++ clockify-app.rb
@@ -1,4 +1,5 @@
 require "time_difference"
+require "date_format"
 
 class Interface
     
@@ -10,21 +11,38 @@ class Interface
         puts "[1] Add Task [2] Dashboard"
     end
 
+    def self.task_list()
+        p Task.data
+        gets
+        for i in Task.data
+            puts "----------"
+            puts "#{i.name} (#{i.category})"
+            puts "#{i.description}"
+            puts "Start: #{i.start_time}"
+            puts "End: #{i.end_time}"
+            puts "Duration: #{TimeDifference.between(i.start_time, Time.now).humanize}"
+        end 
+    end
+
 end
 
 class Task 
+    attr_reader :name, :category, :description, :start_time, :end_time
 
-    @@task_data []
+    @@data = []
 
     def initialize(name, category, description, start_time, end_time)
         @name = name
         @category = category
         @description = description
-        @start_time = start_time
-        @end_time = end_time
+        @start_time = DateFormat.change_to(start_time, "MEDIUM_TIME")
+        @end_time = "ongoing"
+        @duration = 0
     end
 
-    def add_task()
+    def self.add_task()
+        system("clear")
+        Interface.header_display()
         print "Task Name: "
         name = gets.chomp.to_s
         print "Category: "
@@ -32,16 +50,30 @@ class Task
         print "Description: "
         description = gets.chomp.to_s
         start_time = Time.now
-        Task.new(name, category, description, start_time, 0)
+        task = Task.new(name, category, description, start_time, 0)
+        Task.data.push(task)
+    end
+
+    def self.data()
+        @@data
     end
 
 end
 
-# time1 = Time.now
-# sleep 5
-# time2 = Time.now
+system("clear")
 
-# diff = TimeDifference.between(time1, time2).humanize
+app_on = true
+while app_on
 
-# puts diff
+    Interface.header_display()
+    Interface.task_list()
+    Interface.control_panel()
+    answer = gets.chomp.to_s
+    case answer
+    when "1"
+        Task.add_task()
+    end
+
+end
+
 

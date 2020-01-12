988721237d20ba77f9cbcca27d06eebf0f98d641
Caleb Leung <caleb.leungg@gmail.com>
2020-01-12 20:01:18 +1000

    added feature for displaying ongoing entry on main page if currently running- is reflective of live status on website also

--- controllers/clockify-controller.rb
+++ controllers/clockify-controller.rb
@@ -6,22 +6,24 @@ class ClockifyController
 
     def initialize
         @clockify_view = ClockifyView.new()
+        @clockify_view.title
+        @clockify_model = ClockifyModel.new(@clockify_view.get_api_key)
+        @auth = @clockify_model.auth
+        @workspace_id = @clockify_model.workspace_id
+        @user_id = @clockify_model.user_id
     end
 
     def start
-        @clockify_view.title
-        @clockify_model = ClockifyModel.new(@clockify_view.get_api_key)
         loop do
             system("clear")
             @clockify_view.title
+            @clockify_view.display_ongoing_entry(@clockify_model.get_entries_data(@auth, @workspace_id, @user_id))
             @clockify_view.control_panel
             input = gets.chomp
             case input
             when "2"
-                @clockify_view.list_entries(@clockify_model.get_entries_data(@clockify_model.auth, @clockify_model.workspace_id, @clockify_model.user_id))
-                puts "---"
-                puts "Press any key to return"
-                gets
+                @clockify_view.list_entries(@clockify_model.get_entries_data(@auth, @workspace_id, @user_id))
+                @clockify_view.back_prompt
             when "x"
                 break
             end
--- views/clockify-view.rb
+++ views/clockify-view.rb
@@ -1,11 +1,19 @@
 class ClockifyView
     
     def title
-        puts "------ terminal Clockify ------\n\n"
+        puts "------ terminal Clockify ------"
+        puts
+        puts
     end
 
     def control_panel
         puts "[1] Add Task [2] Dashboard [3] Refresh [x] Quit"
+    end
+
+    def back_prompt
+        puts "---"
+        puts "Press Enter key to return"
+        gets
     end
 
     def get_api_key
@@ -22,6 +30,23 @@ class ClockifyView
             puts "End Time: #{entry["timeInterval"]["end"]}"
             puts "Duration: #{entry["timeInterval"]["duration"]}"
             puts "------"
+        end
+    end
+
+    def display_ongoing_entry(data)
+        if data[0]["timeInterval"]["end"] == nil
+            puts "------------------------------------------"
+            puts "Description: #{data[0]["description"]}"
+            puts "Start Time: #{data[0]["timeInterval"]["start"]}"
+            puts "Status: Ongoing"
+            puts "------------------------------------------"
+            puts
+        else
+            puts "------------------------------------------"
+            puts "No Ongoing Entries"
+            puts "Select [1] to start a new Entry"
+            puts "------------------------------------------"
+            puts
         end
     end
 
